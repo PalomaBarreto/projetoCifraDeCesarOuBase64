2dc7382671ff6ec7847b035e65230010c8b79fa3
PalomaBarreto <palomaufrrj@gmail.com>
2021-09-01 00:32:35 -0300

    correções no arquivo javascript

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var tipoCodificador = document.getElementById('tipoDeCodificador')
+var tipoDeCodificador = document.getElementById('tipoDeCodificador')
 var selecionaCodi = document.getElementById('radioCodificar')
 var selecionaDecodi = document.getElementById('radioDecodificar')
 var botao = document.getElementById('submeter')
+var msgDecifrada = ''
 
-//Evento que vai remover ou adiciovar a classe com display none. Mostrar o campo da "chave" quando estiver selecionado Cifra de César.
-tipoCodificador.addEventListener('change', function (e)
+//evento que vai remover ou adiciovar a classe com display none. Mostrar o campo da "chave" quando estiver selecionado Cifra de César.
+tipoDeCodificador.addEventListener('change', function (e)
 {
-  var metodo = tipoCodificador.value
+  var metodo = tipoDeCodificador.value
   var selecionaLabelChave = document.getElementById('labelChave').classList
   var selecionaInputChave = document.getElementById('inputChave').classList
   
@@ -20,98 +21,94 @@ tipoCodificador.addEventListener('change', function (e)
     selecionaInputChave.remove('chaveInvisivel')
     selecionaLabelChave.remove('chaveInvisivel')
   }
-  
 })
-
 
 //evento que mostra "codificar a mensagem" quando o radio estiver selecionado em codificar
 selecionaCodi.addEventListener('click', function (e) 
 {
-  botao.innerText = "codificar a mensagem"   
-  
+  botao.innerText = "codificar a mensagem"  
 })
 
 //evento que mostra "decodificar a mensagem" quando o radio estiver selecionado em decodificar
 selecionaDecodi.addEventListener('click', function (e) 
 {
-  botao.innerText = "decodificar a mensagem"
-  
+  botao.innerText = "decodificar a mensagem"  
 })
 
-//função que codifica ou decodifica em cifra de césar
-function cifra(texto, passo) {
-  var mensagemInicial = document.getElementById('mensagem').value
+//codifica ou decodifica em cifra de césar
+function cifra(texto, passo)
+{
+  var msgInicial = document.getElementById('mensagem').value
+  var msgFinal = document.getElementById('resultado')
   var chave = parseInt((document.getElementById('inputChave').value))
-  var mensagemFinal = document.getElementById('resultado')
-  var mensagemInicialSeparada = mensagemInicial.split("")
-  var mensagemSeparadaEmNum = []
-  var mensagemSeparadaEmString = []
-  var mensagemDecifrada = ""
+  var msgInicialSeparada = msgInicial.split("")
+  var msgSeparadaEmNum = []
+  var msgSeparadaEmString = []
   var rotacao = ''
-
+  
   if (selecionaCodi.checked) 
   {
     rotacao = chave
   }
-
+  
   else 
   {
     rotacao = (-1)*(chave)
   }
   
-  for (var i = 0; i < mensagemInicialSeparada.length; i++) 
+  for (var i = 0; i < msgInicialSeparada.length; i++) 
   {
-    mensagemSeparadaEmNum.push(mensagemInicialSeparada[i].charCodeAt() + (parseInt(rotacao)))
+    if (msgInicialSeparada[i].charCodeAt() >= 65 && msgInicialSeparada[i].charCodeAt() <= 90)
+    {
+      msgSeparadaEmNum.push(((msgInicialSeparada[i].charCodeAt() - 90 + (parseInt(rotacao))) % 26 ) + 90)
+    }
+    
+    else if (msgInicialSeparada[i].charCodeAt() >= 97 && msgInicialSeparada[i].charCodeAt() <= 122) 
+    {
+      msgSeparadaEmNum.push(((msgInicialSeparada[i].charCodeAt() - 122 + (parseInt(rotacao))) % 26 ) + 122)
+      console.log(msgSeparadaEmNum);
+    }
+    
+    else
+    {
+      msgSeparadaEmNum.push(msgInicialSeparada[i].charCodeAt())
+    }
   }
   
-  for (var j = 0; j < mensagemSeparadaEmNum.length; j++) 
+  for (var j = 0; j < msgSeparadaEmNum.length; j++) 
   {
-    mensagemSeparadaEmString.push(String.fromCharCode(mensagemSeparadaEmNum[j]))
-    
+    msgSeparadaEmString.push(String.fromCharCode(msgSeparadaEmNum[j]))    
   }
   
-  mensagemDecifrada = mensagemSeparadaEmString.join('')
-  mensagemFinal.value = `${mensagemDecifrada}`
-    
+  msgDecifrada = msgSeparadaEmString.join('')
+  msgFinal.value = `${msgDecifrada}`
 }
 
-//função que codifica ou decodifica em base64
+//codifica ou decodifica em base64
 function base64(texto) 
 {
-  var escolha = ""
-  var mensagemInicial = document.getElementById('mensagem').value
-  var mensagemFinal = document.getElementById('resultado')
-  var codi64 = window.btoa(mensagemInicial)
-  var decodi64 = window.atob(mensagemInicial)
+  var msgInicial = document.getElementById('mensagem').value
+  var msgFinal = document.getElementById('resultado')
+  var codi64 = window.btoa(msgInicial)
+  var decodi64 = window.atob(msgInicial)
   
   if (selecionaCodi.checked) 
   {
-    escolha = 'codificar'
+    msgFinal.value = `${codi64}`  
   }
   
-  else
+  else if (selecionaDecodi.checked)
   {
-    escolha = 'decodificar'
+    msgFinal.value = `${decodi64}`
   }
-
-  if (escolha === 'codificar') 
-  {
-    mensagemFinal.value = `${codi64}`
-  }
-
-  else if (escolha === 'decodificar') 
-  {
-    mensagemFinal.value = `${decodi64}`
-  }
-     
 }
 
 //evento que escuta o botao de submeter e dispara as funções para decifrar
 botao.addEventListener('click', function (e) 
 {
   e.preventDefault()
-  var metodo = tipoCodificador.value
-    
+  var metodo = tipoDeCodificador.value
+  
   if (metodo === "cifra")
   {
     cifra()
@@ -121,6 +118,4 @@ botao.addEventListener('click', function (e)
   {
     base64()
   }
- 
 })
-
